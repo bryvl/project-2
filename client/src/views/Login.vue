45747226082e8d167ebcbcd42eb079dc1f864a05
bcoggins78 <bcoggins78@gmail.com>
2019-05-20 00:55:35 -0400

    Added googlelogin component to login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,42 +1,49 @@
 <template>
   <div class="login">
-        <form @submit.prevent="doLogin">
-            <p>
-              <label for="name-input">
-                  <span>User Name:  </span>
-                  <input v-model="name" type="text" id="name-input" name="name-input">
-              </label>
-            </p>
-            <p>
-              <label for="email-input">
-                  <span>Email:  </span>
-                  <input v-model="email" type="text" id="email-input" name="email-input">
-              </label>
-            </p>
-            <button @click='gohome' type="submit">
-                Login
-            </button>
-        </form>
+    <GoogleLogin/>
+    <form @submit.prevent="doLogin">
+      <p>
+        <label for="name-input">
+          <span>User Name:</span>
+          <input v-model="name" type="text" id="name-input" name="name-input">
+        </label>
+      </p>
+      <p>
+        <label for="email-input">
+          <span>Email:</span>
+          <input v-model="email" type="text" id="email-input" name="email-input">
+        </label>
+      </p>
+      <button @click="gohome" type="submit">Login</button>
+    </form>
   </div>
 </template>
 
 <script>
+import GoogleLogin from '@/components/GoogleLogin';
+
 export default {
-  name: 'Login',
-    data: function() {
-      return {
-          name: '',
-          email: ''
-      }
+  name: "Login",
+  data: function() {
+    return {
+      name: "",
+      email: ""
+    };
+  },
+  components: {
+    GoogleLogin
   },
   methods: {
     gohome: function() {
-      this.$router.push('/');
+      this.$router.push("/");
     },
     doLogin: function() {
-            localStorage.setItem('name', this.name);
-            localStorage.setItem('email', this.email);
-        }
+      localStorage.setItem("name", this.name);
+      localStorage.setItem("email", this.email);
+    },
+    onUserLoggedIn (googleUser) {
+      console.log(googleUser)
+    }
   }
-}
+};
 </script>
