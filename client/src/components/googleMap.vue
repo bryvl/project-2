0221bb901503769df3cf221fa26db742a57e1463
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-05-31 18:56:32 -0400

    committing small change to feed

--- client/src/components/googleMap.vue
+++ client/src/components/googleMap.vue
@@ -1,46 +1,130 @@
 <template>
   <div class="app">
    <GmapMap
-  :center="{lat:40, lng:-100}"
-  :zoom="3"
+ v-bind:center="{lat: userLat, lng: userLng}"
+  :zoom="15"
   map-type-id="terrain"
   style="width: 500px; height: 300px"
 >
   <GmapMarker
     :key="index"
     v-for="(m, index) in markers"
-    :position="m.position"
+    v-bind:position="m.position"
     :clickable="true"
     :draggable="false"
     @click="center=m.position"
     />
 
 </GmapMap>
+<div @mouseover="callGeocode()">MOUsEOVER</div>
+<h1> {{ userLat }}  </h1>
+<h1> {{ userLng }} </h1>
+
   </div>
 </template>
 <script>
 
-
+// import mapfile from '../maps.js';
+// var maps = require('../maps.js');
+// import mapsapi from '../maps.js';
+// let geocode = require('../maps.js');
+// import geocode from "../maps.js";
+import main from '../main.js';
+var axios = require ('axios');
+// var userLat;
+// var userLng;
+// console.log("will this lat work?" + maps.geocode());
+var location="Raleigh, NC"
+// console.log(.youThere);
 export default {
     name: "GoogleMap",
-  data() {
-    return {
-      //Below the center needs to be userLat and userLong
-      center: { lat: -3.350235, lng: 111.995865 },
+     data: function() {
+      return {
+      userLat: 0,
+      userLng: 0,
+      //The center below should not have affect on map
+      // center: { lat: 42.7796, lng:-78.63},
       mapTypeId: "terrain",
       markers: [
           //THESE MARKERS CAN BE CHANGED TO THE POSITION OF EVENT
           //HOWEVER, WE NEED TO GEOCODE THEM TO ADDRESS FORM
           //markers below also have to be userLat and userLong so it represents event.
-        { position: { lat: 35.7796, lng: -78.63 } },
-        { position: { lat: -6.9127778, lng: 107.6205556 } }
+        { position: { lat:0 , lng:0  } }
       ]
       
       
     };
+  
+  },
+    methods:{
+      callGeocode() {
+        console.log("callgeocode is being called")
+    console.log("geocode is being called");
+    location = '1209 inlet place';
+   
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+            address:location,
+            key:'AIzaSyDFRyu277w4V5p1mjxGseqqo1yRXqBRbBw'
+        }
+    })
+    .then(function(response){
+       //log response
+    // console.log(response);
     
-  }
-};
+    // formatted address
+    console.log(response.data.results[0].formatted_address);
+
+    // lat
+    // console.log(response.data.results[0].geometry.location.lat);
+    this.userLat = response.data.results[0].geometry.location.lat;
+    // console.log("The latitude is:" + userLat)
+
+    // long
+    // console.log(response.data.results[0].geometry.location.lng);
+    this.userLng = response.data.results[0].geometry.location.lng;
+    // console.log("The longitude is:" + userLng)
+
+// var mapData= {
+//     userLat: userLat,
+//     userLng: userLng,
+// }
+
+   console.log("userlat:" + this.userLat)
+      console.log("userlng:" + this.userLng)
+}.bind(this))
+.catch(function(error){
+    console.log("this didn't work", error);
+}
+);
+      }
+    },
+    
+  mounted() {
+    // var userLat = this.callGeocode();
+    // var userLng = this.callGeocode();
+    this.callGeocode();
+    
+  },
+   watch: {userLat: function() {
+     this.markers[0].position.lat = this.userLat;
+   },
+    userLng: function() {
+         this.markers[0].position.lng = this.userLng;
+    }
+    // location: function() {
+    //   this.
+    // }
+    },
+
+  //   {
+  //   lat: function(newValue) {
+  //     this.gmap.panTo({
+  //       userLat: parseFloat(newValue),
+  //       userLng: parseFloat(this.userLng)
+  //     });
+    }
+   ;
 </script>
 
 <style lang="scss" scoped>
--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="feed">
       <h1>Test Feed Works</h1>
+
+<GoogleMap/>
+
   </div>
 
 </template>
 
 <script>
+import GoogleMap from "@/components/GoogleMap";
+
 export default {
-  name: 'Feed'
+  name: 'Feed',
+  components:{
+  GoogleMap
 }
+}
+
 
 </script>
 
